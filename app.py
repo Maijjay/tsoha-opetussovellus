1790e9369a3faed8e68eb3e2a37280f40a9a2244
Maijjay <maija.ylitalo95@gmail.com>
2021-09-27 00:02:35 +0300

    db uri fix.

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ from sqlalchemy.exc import IntegrityError
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY") 
+
+# Fix db sql alchemy not accepting postgres://
+db_uri = getenv("DATABASE_URL")
+if db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
